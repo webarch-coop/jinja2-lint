b5d27fb2efd1a2dbedca5e4aa43f7778671b85f4
Chris Croome <chris@webarchitects.co.uk>
2023-04-21 23:21:36 +0100

    fix for https://github.com/drm/jinja2-lint/issues/18

--- j2lint.py
+++ j2lint.py
@@ -9,9 +9,7 @@ import os.path
 from functools import reduce
 from jinja2 import BaseLoader, TemplateNotFound, Environment, exceptions, filters
 from jinja2_ansible_filters import AnsibleCoreFiltersExtension
-from ansible_collections.ansible.netcommon.plugins.filter import ipaddr
 
-filters.FILTERS['ipaddr'] = ipaddr
 class AbsolutePathLoader(BaseLoader):
     def get_source(self, environment, template):
         if not os.path.exists(template):
